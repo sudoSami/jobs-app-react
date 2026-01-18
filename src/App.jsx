10b96d717dd3755568b528faf9b2c92385dad94e
babakaana <samiulhaq744@gmail.com>
2026-01-18 18:38:38 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Hero from "./components/Hero"
+import HomeCards from "./components/HomeCards"
+import JobListings from "./components/JobListings"
+import Navbar from "./components/Navbar"
+import ViewAllJobs from "./components/ViewAllJobs"
+
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  )
+}
+
+export default App
