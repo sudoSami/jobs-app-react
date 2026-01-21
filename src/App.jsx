2c0a783c3a9e91ae9e3b753283cb492b578d30ba
babakaana <samiulhaq744@gmail.com>
2026-01-21 17:52:38 +0500

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import Hero from "./components/Hero"
-import HomeCards from "./components/HomeCards"
-import JobListings from "./components/JobListings"
-import Navbar from "./components/Navbar"
-import ViewAllJobs from "./components/ViewAllJobs"
-
-
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MainLayout from './Layouts/MainLayout';
 const App = () => {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<HomePage />}></Route>
+      </Route>
+    )
+  )
+
   return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
+    <RouterProvider router={router}>  
+    </RouterProvider>
   )
 }
 
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { useState } from 'react'
+import {FaMapMarker} from 'react-icons/fa'
 
 const JobListing = ({ job }) => {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = job.description;
+
+  if (!showFullDescription) {
+    description = description.substring(0, 90) + '...';
+  }
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
             <div className="p-4">
@@ -10,8 +20,11 @@ const JobListing = ({ job }) => {
               </div>
 
               <div className="mb-5">
-               {job.description}
+               {description}
               </div>
+
+              <button onClick={()=>setShowFullDescription((prev)=>!prev)}
+               className='text-indigo-500 mb-5 hover:text-indigo-600'>{showFullDescription? 'Less': 'More'}</button>
 
               <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
@@ -19,7 +32,7 @@ const JobListing = ({ job }) => {
 
               <div className="flex flex-col lg:flex-row justify-between mb-4">
                 <div className="text-orange-700 mb-3">
-                  <i className="fa-solid fa-location-dot text-lg"></i>
+                  <FaMapMarker className='inline mr-1 ml-1 mb-1 text-lg' />
                   {job.location}
                 </div>
                 <a
